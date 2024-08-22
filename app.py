2ffd2c3698bf6df24b8a91ec29c1c07fce3b94c3
mkali <getskanice@gmail.com>
2024-08-22 22:50:03 +0200

    first init

--- app.py
+++ app.py
@@ -1,34 +1,52 @@
-from flask import Flask, render_template, request, jsonify
-from flask_socketio import SocketIO, emit
+import os
+import json
 import subprocess
-import threading
 import shlex
-import re
-import yaml
+from flask import Flask, jsonify, request
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
 socketio = SocketIO(app)
 
+OUTPUT_DIR = 'outputs'
+if not os.path.exists(OUTPUT_DIR):
+    os.makedirs(OUTPUT_DIR)
+
 running_commands = {}
 
-# Load allowed tools and workflows from YAML files
-with open('allowed_tools.yaml', 'r') as f:
-    config = yaml.safe_load(f)
-    allowed_tools = set(config['allowed_tools'])
+def save_output(command_id, output):
+    filepath = os.path.join(OUTPUT_DIR, f'{command_id}.json')
+    if os.path.exists(filepath):
+        with open(filepath, 'r') as file:
+            data = json.load(file)
+    else:
+        data = {'output': []}
+    
+    data['output'].append(output)
+    
+    with open(filepath, 'w') as file:
+        json.dump(data, file)
 
-with open('workflows.yaml', 'r') as f:
-    workflows = yaml.safe_load(f)['workflows']
+def load_output(command_id):
+    filepath = os.path.join(OUTPUT_DIR, f'{command_id}.json')
+    if os.path.exists(filepath):
+        with open(filepath, 'r') as file:
+            data = json.load(file)
+        return data.get('output', [])
+    return []
 
-def clean_output(output):
-    ansi_escape = re.compile(r'\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])')
-    cleaned = ansi_escape.sub('', output)
-    return ''.join(char for char in cleaned if ord(char) >= 32 or char in '\n\r\t')
+@app.route('/output/<command_id>')
+def get_output(command_id):
+    output = load_output(command_id)
+    return jsonify({'output': output})
 
-def is_tool_allowed(command):
-    tool = command.split()[0]
-    return tool in allowed_tools
+@app.route('/run_command', methods=['POST'])
+def run_command():
+    command_id = request.json.get('id')
+    command = request.json.get('command')
+    if not command_id or not command:
+        return jsonify({'error': 'Missing command or ID'}), 400
 
-def run_command(command_id, command):
     process = subprocess.Popen(shlex.split(command), stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True, bufsize=1, universal_newlines=True)
     running_commands[command_id] = process
     
@@ -36,8 +54,9 @@ def run_command(command_id, command):
     socketio.emit('command_started', {'id': command_id, 'command': command, 'tool': tool})
 
     for line in process.stdout:
-        cleaned_line = clean_output(line.strip())
+        cleaned_line = line.strip()
         if cleaned_line:
+            save_output(command_id, cleaned_line)  # Save output to a file
             socketio.emit('command_output', {'id': command_id, 'output': cleaned_line})
 
     process.wait()
@@ -47,52 +66,17 @@ def run_command(command_id, command):
         socketio.emit('command_completed', {'id': command_id, 'status': 'error'})
 
     del running_commands[command_id]
+    return jsonify({'status': 'Command started'})
 
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/workflows')
-def get_workflows():
-    return jsonify(workflows)
-
-@socketio.on('run_command')
-def handle_command(data):
-    command_id = data['id']
-    command = data['command']
-
-    if is_tool_allowed(command):
-        threading.Thread(target=run_command, args=(command_id, command)).start()
-    else:
-        tool = command.split()[0]
-        socketio.emit('command_error', {'id': command_id, 'error': f"Tool '{tool}' is not allowed to run."})
-
-@socketio.on('run_workflow')
-def handle_workflow(data):
-    command_id = data['id']
-    workflow_name = data['workflow']
-    domain = data['domain']
-
-    if workflow_name in workflows:
-        workflow = workflows[workflow_name]
-        for step in workflow['steps']:
-            command = step['command'].format(domain=domain, url=f"http://{domain}")
-            if is_tool_allowed(command):
-                # Send the command with an updated ID to keep track of the output
-                new_command_id = f"{command_id}-{step['tool']}-{workflow['steps'].index(step)}"
-                threading.Thread(target=run_command, args=(new_command_id, command)).start()
-            else:
-                socketio.emit('command_error', {'id': command_id, 'error': f"Tool '{step['tool']}' is not allowed."})
-                break
-    else:
-        socketio.emit('command_error', {'id': command_id, 'error': f"Workflow '{workflow_name}' not found."})
-
-@socketio.on('stop_command')
-def stop_command(data):
-    command_id = data['id']
+@app.route('/stop_command', methods=['POST'])
+def stop_command():
+    command_id = request.json.get('id')
     if command_id in running_commands:
-        running_commands[command_id].terminate()
+        process = running_commands.pop(command_id)
+        process.terminate()
         socketio.emit('command_stopped', {'id': command_id})
+        return jsonify({'status': 'Command stopped'})
+    return jsonify({'error': 'Command not found'}), 404
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
